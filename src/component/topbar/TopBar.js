5dc613e3400b80c6c63cdd58917754b88149b90e
mayurathan22 <thavaranjanmayurathan95@gmail.com>
2021-07-25 17:10:47 +0530

    add_topbar_link

--- src/component/topbar/TopBar.js
+++ src/component/topbar/TopBar.js
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./Topbar.css";
-import profile from "../../assets/profile.jpg"
+import profile from "../../assets/profile.jpg";
 function TopBar() {
+  const user = false;
   return (
     <div className="top">
       <div className=" topLeft">
@@ -11,14 +13,46 @@ function TopBar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">Contact</li>
-          <li className="topListItem">Logout</li>
+          <li className="topListItem">
+            <Link to="/" style={{color:"inherit", textDecoration:"none"}} >
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/about" >
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/contact" >
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/write" className="link">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">
+            {user && "LOGOUT"}
+          </li>
         </ul>
       </div>
       <div className="topRight">
-        <img className="topImg" src={profile} alt="" />
+        {user? (
+          <img className="topImg" src={profile} alt="" />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">REGISTER</Link>
+            </li>
+          </ul>
+        )}
         <i className="topSearchIcon fas fa-search"></i>
       </div>
     </div>
